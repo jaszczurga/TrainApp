beef9eca5bc84bbf974846194f7f0d822a59558e
jaszczurga <goskar2k@gmail.com>
2023-11-17 13:25:10 +0100

    przed loggerem

--- api/routes/trains.js
+++ api/routes/trains.js
@@ -9,10 +9,33 @@ router.get("/",(req,res,next)=>{
     })
 })
 
-router.get("/",(req,res,next)=>{
-    res.status(200).json({
+router.post("/",(req,res,next)=>{
+    res.status(201).json({
         wiadomosc: "utworzenie nowego pociagu"
     })
 })
+
+router.get("/:id",(req,res,next)=>{
+    const id = req.params.id
+    res.status(200).json({wiadomosc: "szczegoly o numerze "+id})
+})
+
+router.put("/:id",(req,res,next)=>{
+    const id = req.params.id
+    res.status(200).json({wiadomosc: "zmiana danych o numerze "+id})
+})
+router.delete("/:id",(req,res,next)=>{
+    const id = req.params.id
+    res.status(200).json({wiadomosc: "usuniecie danych o numerze "+id})
+})
+
+
+// create -> POST /trains -> utworzenie pociagu nowego
+// read -> GET /trains/100 -> info o pocaigu o nimerze 100
+// update -> PUT /trains/100 -> zmiana pocaigu o numerze 100
+// delet -> DELETE /trains/100 -> usuwa pociag o numerze 100
+
+//nodemon
+
 
 module.exports=router
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const trainRoutes = require("./api/routes/trains")
 //instancja expressa
 const app = express()
 
+
+
 app.use("/trains",trainRoutes)
 app.use((req,res,next)=>{
     res.status(200).json({wiadomosc:"Wszystko smiga"})
