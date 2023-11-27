5f52b3e9893d61ab1faf2135362288a35fda6f08
jaszczurga <goskar2k@gmail.com>
2023-11-27 14:03:19 +0100

    parsowanie w app.js i npm i body-parser

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const trainRoutes = require("./api/routes/trains")
 //instancja expressa
 const app = express()
 
+//logger
 const morgan = require("morgan")
 app.use(morgan("combined"))
 
+//parsowanie body
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+
 app.use("/trains",trainRoutes)
 app.use((req,res,next)=>{
-    res.status(200).json({wiadomosc:"Wszystko smiga"})
+    res.status(404).json({wiadomosc:"Wszystko smiga"})
 })
 //exportuje app
 module.exports = app
